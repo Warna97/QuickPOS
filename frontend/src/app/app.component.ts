295a57720fc26b250d019be1e89a7fc3e1315dc9
Lahiru  Senevirathne <76489840+Warna97@users.noreply.github.com>
2024-11-01 18:51:24 +0000

    update app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,28 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Use CommonModule for standalone components
 import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    CommonModule, // Use CommonModule instead of BrowserModule
     RouterOutlet,
-    BrowserAnimationsModule, 
     MatTableModule,
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
   ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'frontend';
